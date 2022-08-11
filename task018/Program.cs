afc6e4c1dcd23dc975333dabd0244d04e4488b42
Vyacheslav <grinksic@gmail.com>
2022-08-11 21:15:42 +0300

    task 018 is done

---
+++ task018/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер координатной четверти: ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+if (X == 1)
+{
+    Console.WriteLine("X > 0, Y > 0");
+}
+else if (X == 2)
+{
+    Console.WriteLine("X < 0, Y > 0");
+}
+else if (X == 3)
+{
+    Console.WriteLine("X < 0, Y < 0");
+}
+else if (X == 4)
+{
+    Console.WriteLine("X > 0, Y < 0");
+}
+else
+{
+    Console.WriteLine("Введите повторно номер координатной четверти от 1 до 4");
+}
