237b8a3f11046352527f035ad28740c85c775cdd
Vyacheslav <grinksic@gmail.com>
2022-08-15 20:30:15 +0300

    added new solution for task021

--- task021/Program.cs
+++ task021/Program.cs
@@ -9,9 +9,11 @@
 Console.WriteLine("Введите пятизначное число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
+string numberText = Convert.ToString(N); // общая конвертация в текст, чтобы потом не делать конвертацию в каждой строке
+
 if (N > 9999 && N < 100000)
 {
-   if ( N.ToString()[0] == N.ToString()[4] && N.ToString()[1] == N.ToString()[3] )
+   if ( numberText[0] == numberText[4] && numberText[1] == numberText[3] )
    {
    Console.WriteLine($"{N} -> да");
    }
